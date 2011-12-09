1e1efccbe117661ceb5441aefce5cfc654f0bcf2
Tay Ray Chuan <rctay89@gmail.com>
2011-12-09 13:13:23 +0800

    do away with redundant TestRakeDirectoryTask.test_directory_win32
    Most of what is tested is already covered in the other 2 tests - ie.
    lookup of slashed/non-slashed directory tasks, absence of comments on
    "parent" tasks, and laziness of directory creation.
    
    test_directory_win32 also looks at absolute paths (by prepending
    Dir.pwd), but we don't have to worry about this case since it boils down
    to the correctness about File.dirname; leave that to ruby core
    developers. :)

--- test/test_rake_directory_task.rb
+++ test/test_rake_directory_task.rb
@@ -44,34 +44,4 @@ class TestRakeDirectoryTask < Rake::TestCase
     refute_instance_of Rake::FileTask,         Task["a"]
     assert_instance_of Rake::FileCreationTask, Task["a"]
   end
-
-  if Rake::Win32.windows?
-    def test_directory_win32
-      drive = Dir.pwd
-      while drive != File.dirname(drive)
-        drive = File.dirname(drive)
-      end
-      drive = drive[0...-1] if drive[-1] == ?/
-
-      desc "WIN32 DESC"
-      directory File.join(Dir.pwd, 'a/b/c')
-
-      if drive[-1] == ?:
-        assert_equal FileTask, Task[drive].class
-        assert_equal FileCreationTask, Task[drive + ?/].class
-      end
-
-      assert_equal FileCreationTask, Task[File.join(Dir.pwd, 'a')].class
-      assert_equal FileCreationTask, Task[File.join(Dir.pwd, 'a/b')].class
-      assert_equal FileCreationTask, Task[File.join(Dir.pwd, 'a/b/c')].class
-      assert_nil             Task[drive].comment
-      assert_equal "WIN32 DESC",   Task[File.join(Dir.pwd, 'a/b/c')].comment
-      assert_nil             Task[File.join(Dir.pwd, 'a/b')].comment
-      verbose(false) {
-        Task[File.join(Dir.pwd, 'a/b')].invoke
-      }
-      assert File.exist?(File.join(Dir.pwd, 'a/b'))
-      refute File.exist?(File.join(Dir.pwd, 'a/b/c'))
-    end
-  end
 end
