69deee8daed3550d8326ddaec5f4e6aa4f655a61
Michael Bishop <mbtyke@yahoo.com>
2012-04-16 06:54:29 -0400

    Rake now limits the number of threads to 16
    This change hardcodes the limit to 16 as a proof-of-concept. Further
    changes will read a parameter (likely "-j") from the command line to
    determine the maximum number.

--- lib/rake/multi_task.rb
+++ lib/rake/multi_task.rb
@@ -1,15 +1,76 @@
+require 'thread'
+require 'set'
+
 module Rake
 
   # Same as a regular task, but the immediate prerequisites are done in
   # parallel using Ruby threads.
   #
   class MultiTask < Task
+    @@multi_task_queue = Queue.new
+    @@thread_group = ThreadGroup.new
+  
     private
+
     def invoke_prerequisites(args, invocation_chain)
-      threads = @prerequisites.collect { |p|
-        Thread.new(p) { |r| application[r, @scope].invoke_with_call_chain(args, invocation_chain) }
-      }
-      threads.each { |t| t.join }
+
+        # bookkeeping
+        bookkeeping_task_count = 0
+        bookkeeping_threads = Set.new
+        bookkeeping_semaphore = Mutex.new
+        
+        @prerequisites.each do |r|
+
+          bookkeeping_semaphore.synchronize { bookkeeping_task_count += 1 }
+
+          @@multi_task_queue.push lambda {
+
+            bookkeeping_semaphore.synchronize { bookkeeping_threads.add Thread.current }
+
+            application[r, @scope].invoke_with_call_chain(args, invocation_chain)
+
+            bookkeeping_semaphore.synchronize { 
+              bookkeeping_task_count -= 1
+              bookkeeping_threads.delete Thread.current
+            }
+          }
+          
+          # Here, we only create a new thread if we are under the max number of threads
+          if @@thread_group.list.count >= 16
+            next
+          end
+
+          @@thread_group.add Thread.new {
+            begin
+
+              while something = @@multi_task_queue.pop(true)
+                something.call
+              end
+
+            rescue ThreadError
+            end
+          }
+        end
+
+        # while we wait for our tasks to complete, we process tasks ourselves to avoid
+        # deadlock.
+        # If there are no more blocks for use to execute and our tasks are stil not
+        # completed, it's because there are other threads still working on our tasks
+        # since we know the set of threads that are currently working on our tasks
+        # we join them and wait for them to finish
+        
+        while (bookkeeping_task_count > 0)
+            begin
+
+              while something = @@multi_task_queue.pop(true)
+                something.call
+              end
+            rescue ThreadError
+                threads_copy = nil
+                bookkeeping_semaphore.synchronize { threads_copy = bookkeeping_threads.dup }
+                threads_copy.each {|thread| thread.join}
+            end
+        end
     end
   end
 
