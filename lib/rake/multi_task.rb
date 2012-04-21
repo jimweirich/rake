0ee43db363a98bd25be2fb725bdd34c3eb7ba527
Michael Bishop <mbtyke@gmail.com>
2012-04-21 00:09:59 -0400

    Added -m option which forces every task to be a multitask
    This change pulls the concurrent implementation of
    'invoke_prerequisites' out of MultiTask into Task while changing the
    method name to 'invoke_prerequisites_concurrently'
    
    Then, if -m is passed, Task calls 'invoke_prerequisites_concurrently'
    so everything is multithreaded.
    
    MultiTask always calls 'invoke_prerequisites_concurrently'

--- lib/rake/multi_task.rb
+++ lib/rake/multi_task.rb
@@ -6,43 +6,11 @@ module Rake
   # Same as a regular task, but the immediate prerequisites are done in
   # parallel using Ruby threads.
   #
-  # invoke_prerequisites delegates processing of the prerequisites to
-  # threads in a thread pool until the thread pool is full.
-  # Then, execution of the prerequisites synchronously continues on,
-  # checking the size of the thread pool after each one just in case
-  # another thread has exited and it can delegate again.
-  #
-  # When all the prerequisites have been called, the current thread
-  # waits for the other threads processing the prerequisites
-  #
   class MultiTask < Task
     private
-    def invoke_prerequisites(args, invocation_chain) # :nodoc:
-      @@thread_pool   ||= Set.new
-      our_threads = Set.new
-      mutex = Mutex.new
-
-      @prerequisites.each do |p|
-        block = lambda {
-          application[p, @scope].invoke_with_call_chain(args, invocation_chain)
-        }
-        if ( @@thread_pool.size < application.options.thread_pool_size )
-          mutex.synchronize {
-            thread = Thread.new do
-              block.call
-              mutex.synchronize {
-                our_threads.delete(thread)
-                @@thread_pool.delete(thread)
-              }
-            end
-            our_threads.add(thread)
-            @@thread_pool.add(thread)
-          }
-        else
-          block.call
-        end
-      end
-      mutex.synchronize { our_threads.dup }.each { |t| t.join }
+    def invoke_prerequisites(task_args, invocation_chain) # :nodoc:
+      invoke_prerequisites_concurrently(task_args, invocation_chain)
     end
+
   end
 end
--- lib/rake/task.rb
+++ lib/rake/task.rb
@@ -171,10 +171,51 @@ module Rake
 
     # Invoke all the prerequisites of a task.
     def invoke_prerequisites(task_args, invocation_chain) # :nodoc:
-      prerequisite_tasks.each { |prereq|
-        prereq_args = task_args.new_scope(prereq.arg_names)
-        prereq.invoke_with_call_chain(prereq_args, invocation_chain)
-      }
+      if ( application.options.always_multitask )
+          invoke_prerequisites_concurrently(task_args, invocation_chain)
+      else
+        prerequisite_tasks.each { |prereq|
+          prereq_args = task_args.new_scope(prereq.arg_names)
+          prereq.invoke_with_call_chain(prereq_args, invocation_chain)
+        }
+      end
+    end
+
+    # invoke_prerequisites_concurrently delegates processing of the
+    # prerequisites to threads in a thread pool until the thread pool
+    # is full. Then, execution of the prerequisites synchronously
+    # continues on, checking the size of the thread pool after each one
+    # just in case another thread has exited and it can delegate again.
+    #
+    # When all the prerequisites have been called, the current thread
+    # waits for the other threads processing the prerequisites
+    #
+    def invoke_prerequisites_concurrently(args, invocation_chain) # :nodoc:
+      @@thread_pool   ||= Set.new
+      our_threads = Set.new
+      mutex = Mutex.new
+
+      @prerequisites.each do |p|
+        block = lambda {
+          application[p, @scope].invoke_with_call_chain(args, invocation_chain)
+        }
+        if ( @@thread_pool.size < application.options.thread_pool_size )
+          mutex.synchronize {
+            thread = Thread.new do
+              block.call
+              mutex.synchronize {
+                our_threads.delete(thread)
+                @@thread_pool.delete(thread)
+              }
+            end
+            our_threads.add(thread)
+            @@thread_pool.add(thread)
+          }
+        else
+          block.call
+        end
+      end
+      mutex.synchronize { our_threads.dup }.each { |t| t.join }
     end
 
     # Format the trace flags for display.
