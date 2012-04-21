578b6377cb6a9ca318d7de3683fe5362c4547128
Michael Bishop <mbtyke@gmail.com>
2012-04-20 23:58:21 -0400

    Fixed bug where the stack was being blown
    The previous "add blocks to queue" method worked, but had the
    unnecessary side-effect of blowing the stack for large amounts of
    prerequisites.
    
    This is a new thread pool implementation which retains all the
    advantages of the original, but keeps the stack size the same as the
    pre-thread-pool rake.
    
    It's closer to the pre-thread-pool rake implementation with the
    addition of checking the thread pool size before spawning a new thread.

--- lib/rake/multi_task.rb
+++ lib/rake/multi_task.rb
@@ -6,47 +6,43 @@ module Rake
   # Same as a regular task, but the immediate prerequisites are done in
   # parallel using Ruby threads.
   #
-  # MultiTasks load all their prerequisites onto a work queue (singleton
-  # for the class) which is processed by a thread pool of variable size.
-  # In order to prevent deadlocks, the current thread also processes
-  # tasks on the queue until its prerequisite tasks are finished.
+  # invoke_prerequisites delegates processing of the prerequisites to
+  # threads in a thread pool until the thread pool is full.
+  # Then, execution of the prerequisites synchronously continues on,
+  # checking the size of the thread pool after each one just in case
+  # another thread has exited and it can delegate again.
+  #
+  # When all the prerequisites have been called, the current thread
+  # waits for the other threads processing the prerequisites
   #
   class MultiTask < Task
     private
-    def invoke_prerequisites(args, invocation_chain)
-      @@block_queue ||= Queue.new
+    def invoke_prerequisites(args, invocation_chain) # :nodoc:
+      @@thread_pool   ||= Set.new
+      our_threads = Set.new
+      mutex = Mutex.new
 
-      block_count = @prerequisites.count
-      block_threads = Set.new
-      blocks_info_semaphore = Mutex.new
-      
-      @prerequisites.each do |r|
-        @@block_queue.enq lambda {
-          blocks_info_semaphore.synchronize { block_threads.add(Thread.current) }
-          application[r, @scope].invoke_with_call_chain(args, invocation_chain)
-          blocks_info_semaphore.synchronize { block_threads.delete(Thread.current); block_count -= 1 }
+      @prerequisites.each do |p|
+        block = lambda {
+          application[p, @scope].invoke_with_call_chain(args, invocation_chain)
         }
-      end
-
-      process_all_blocks_until { block_count == 0 }
-      blocks_info_semaphore.synchronize { block_threads.dup }.each {|thread| thread.join}
-    end
-    
-    def process_all_blocks_until
-      @@thread_pool ||= ThreadGroup.new
-      begin
-        while (!yield && something = @@block_queue.deq(true))
-          # track the thread pool size
-          if @@thread_pool.list.count < application.options.thread_pool_size
-            @@thread_pool.add Thread.new {
-              process_all_blocks_until { @@thread_pool.list.count > application.options.thread_pool_size}
-            }
-          end
-          something.call
+        if ( @@thread_pool.size < application.options.thread_pool_size )
+          mutex.synchronize {
+            thread = Thread.new do
+              block.call
+              mutex.synchronize {
+                our_threads.delete(thread)
+                @@thread_pool.delete(thread)
+              }
+            end
+            our_threads.add(thread)
+            @@thread_pool.add(thread)
+          }
+        else
+          block.call
         end
-      rescue ThreadError
       end
+      mutex.synchronize { our_threads.dup }.each { |t| t.join }
     end
-
   end
 end
